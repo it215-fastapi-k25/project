1010213f90059de0483b86605c6ce3c9b22a2e67
Nguyễn Ngô Quốc Thịnh <nguyenngothinh669@gmail.com>
2026-08-24 21:02:51 +0700

    update code

--- app/core/config.py
+++ app/core/config.py
@@ -4,16 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings): 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8",extra="ignore")
     
-    PROJECT_NAME: str = "Research Group Management API" 
-    ENVIRONMENT : str = "development"
-    DATABASE_URL: str 
-    DB_POOL_SIZE: int = 10
-    DB_MAX_OVERFLOW: int = 20
-    SECRET_KEY: str 
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30 
-    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-    CORS_ORIGINS: list[str] = ["http://localhost:3000", "http://localhost:8000"] 
     
 @lru_cache()
 def get_settings() -> Settings:
